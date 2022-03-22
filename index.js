bfbc08815284de3e65eb126740db51a7daf80684
Emily Phan <nhungphanthi1997@gmail.com>
2022-03-22 13:04:18 +0700

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+        function onChangeColor(){
+            document.getElementById("quote").innerHTML = "Trust yourself. You know more than you think you do.";
+            document.getElementById("quote").style.color = "green";
+            document.getElementById("background").style.backgroundColor = "green";
+            document.getElementById("author").innerHTML = "Dr. Benjamin Spock";
+            document.getElementById("author").style.color = "green";
+            document.getElementById("new-quote-box").style.backgroundColor = "green";
+
+        }
+        
